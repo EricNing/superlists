bf8d8c349f31b749403e4f2793e8bb88478943be
Eric NZ <nz@email.com>
2019-02-12 14:27:09 +0800

    before Website deployment

---
+++ lists/views.py
@@ -0,0 +1,77 @@
+# -*- coding:utf-8 -*-
+from django.shortcuts import render, redirect
+from lists.models import Item, List
+from django.db import connection
+# Create your views here.
+
+
+def get_data_from_ora():
+    import cx_Oracle
+    conn = cx_Oracle.connect('zabbix', 'zabbix', "10.1.23.187:1521/orcl")
+    cursor = conn.cursor()
+    sql = "select clock*1000 clock, round(value*100,2) value from history where itemid=24496 order by clock fetch first 50 rows only"
+    cursor.execute(sql)
+    res = dictfetchall(cursor)
+    cursor.close()
+    conn.close()
+    return res
+
+# def get_data():
+#     import pymysql
+#     conn = pymysql.connect(host='10.1.23.167',
+#                                  user='book',
+#                                  passwd='book',
+#                                  db='book', charset='utf8')
+#     cursor = conn.cursor()
+#     cursor.execute("select * from lists_list")
+#     res = cursor.fetchall()
+#     cursor.close()
+#     conn.close()
+#     return res
+
+def get_lists():
+    with connection.cursor() as cursor:
+        cursor.execute("select * from lists_list")
+        row = dictfetchall(cursor)
+    return row
+
+def dictfetchall(cursor):
+    """
+    :param cursor:
+    :return: Return all rows from a cursor as a dict
+    """
+    columns = [col[0] for col in cursor.description]
+    return [
+        dict(zip(columns, row))
+        for row in cursor.fetchall()
+    ]
+
+def echars(request):
+    # rows = get_data_from_ora
+    return render(request, 'lists/echarts.html')
+                  # , {'row': rows})
+
+def base(request):
+    # 默认去所有APP下的templates目录下查找模板
+    return render(request, 'lists/base.html')
+
+def home_page(request):
+    # 默认去所有APP下的templates目录下查找模板
+    return render(request, 'lists/home.html')
+
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    # items = Item.objects.filter(list=list_)
+    return render(request, "lists/list.html", {'list': list_})
+
+def new_list(request):
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    url = '/lists/' + bytes(list_.id) + '/'
+    return redirect(url)
+
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    url = '/lists/' + bytes(list_.id) + '/'
+    return redirect(url)
